913be0190c2a780e60a75cdf97ff7b255f6f72f8
codemeup <laurakemp@live.co.uk>
2014-12-18 13:27:37 -0800

    it's now updating the proposed_on date

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -28,13 +28,13 @@ class ActivitiesController < ApplicationController
   end
 
   def update
+    @trip = Trip.find(params[:trip_id])
     @activity = Activity.find(params[:id]) #find it
-    puts "activity_params"
-    p activity_params
-    @activity.update_attributes(activity_params)#update attr
+    @activity.update_attributes(:proposed_on => params[:proposed_on])#update attr
     if @activity.save #save it
-      flash[:success] = "Activity updated"
-      redirect_to activities_path
+      # flash[:success] = "Activity updated"
+      # redirect_to trip_activity_path(@trip, @activity)
+      render json: @activity
     else
       render :edit
     end
@@ -46,6 +46,10 @@ class ActivitiesController < ApplicationController
     flash[:success] = "Activity deleted"
     redirect_to activities_path
   end
+
+  # def update_date
+  #   params.require(:activity).permit(:proposed_on)
+  # end
 
   def activity_params
     params.require(:activity).permit(:title, :summary, :link, :category, :address, :zip_code, :booked, :duration, :proposed_on, :booked_on, :created_at, :trip_id, :cost)
